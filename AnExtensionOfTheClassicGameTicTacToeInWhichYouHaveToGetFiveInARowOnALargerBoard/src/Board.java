5274181f4174016a85766bee0e0e20ffa3f85fb2
181hsiehc <38767933+181hsiehc@users.noreply.github.com>
2018-04-26 15:01:41 -0500

    Add files via upload

---
+++ AnExtensionOfTheClassicGameTicTacToeInWhichYouHaveToGetFiveInARowOnALargerBoard/src/Board.java
@@ -0,0 +1,46 @@
+
+public class Board {
+	private int size;
+	private int[][] board;
+	public Board(int s)
+	{
+		size = s;
+		board = new int[size][size];
+		for(int r = 0; r < size; r++)
+		{
+			for(int c = 0; c < size; c++)
+			{
+				board[r][c] = 0;
+			}
+		}
+	}
+	
+	public Board()
+	{
+		size = 15;
+		board = new int[size][size];
+		for(int r = 0; r < size; r++)
+		{
+			for(int c = 0; c < size; c++)
+			{
+				board[r][c] = 0;
+			}
+		}
+	}
+	
+	public int gameWon()
+	{
+		for(int r = 0; r < size; r++)
+		{
+			for(int c = 0; c < size; c++)
+			{
+				int id = board[r][c];
+				if(!c < 4)
+				{
+					
+				}
+			}
+		}
+		return 0;
+	}
+}
